ad50ac90a64b7a5f583c466d5dafbf9a16d05179
Giorgi <beqaurigiorgi31@gmail.com>
2023-10-30 15:45:35 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// important Data that might be changed later 
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = 'ce4bc421-5974-420d-aac9-2820eef889bd';
+export const MODAL_CLOSE_SEC = 2.5
